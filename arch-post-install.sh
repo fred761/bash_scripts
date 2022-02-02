d48a91a267e0da8162406e2ef6fbe57d8b1ea2a9
fred761 <85800170+fred761@users.noreply.github.com>
2022-02-02 18:49:49 +0200

    Update arch-post-install.sh

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -3,9 +3,9 @@
 ### Install minimal gnome session with additional frequently used applications. This script assumes that "archinstall" was used with the default xorg profile.
 
 	pacman -S gdm gnome-session gnome-control-center gnome-tweaks gnome-terminal gedit eog evince fish virt-manager dnsmasq chromium libreoffice-fresh gimp rawtherapee papirus-icon-theme
-	
+
 ### Default gnome settings
-	
+
 	# Disable suspend when laptop lid is closed
 	
 	cd ~
@@ -21,7 +21,7 @@
 	EOF
 
 	# Change default mouse acceleration profile to flat
-
+	
 	cat << EOF | tee > mouse-profile-flat.desktop
 	[Desktop Entry]
 	Type=Application
@@ -39,24 +39,24 @@
 ### Set the governor to performance
 
 	# Create new bash script in /usr/bin
-
+	
 	cd /usr/bin
 	cat << EOF | tee > performance_gov
 	#!/bin/bash
-
+	
 	echo "performance" | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 	EOF
-
+	
 	chmod +x performance_gov
 
 	# Create service file for performance_gov
-
+	
 	cd /etc/systemd/system
 	cat << EOF | tee > performance_gov.service
 	[Unit]
 	Description=Set governor to performance
 	Requires=network.target
-
+	
 	[Service]
 	Type=simple
 	ExecStart=/usr/bin/performance_gov
@@ -68,7 +68,7 @@
 	# Start service
 	
 	systemctl enable performance_gov.service
-	
+
 ### Install Yay
 
 	pacman -S --needed git base-devel
