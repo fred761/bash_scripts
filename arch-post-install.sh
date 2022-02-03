6dc3d2c4d14445f2da55b1010d6dffcd147bf396
fred761 <85800170+fred761@users.noreply.github.com>
2022-02-03 04:26:46 +0200

    Update arch-post-install.sh

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -2,7 +2,7 @@
 
 ### Install minimal gnome session with additional frequently used applications. This script assumes that "archinstall" was used with the default xorg profile.
 
-pacman -Sy gdm gnome-session gnome-control-center gnome-tweaks gnome-terminal nautilus gedit eog evince fish htop gnome-boxes chromium libreoffice-fresh gimp rawtherapee papirus-icon-theme
+pacman -Sy gdm gnome-session gnome-control-center gnome-tweaks gnome-terminal nautilus gedit mc eog evince fish htop gnome-boxes chromium libreoffice-fresh gimp rawtherapee papirus-icon-theme
 
 ### Enable services
 
